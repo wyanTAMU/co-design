d418fcb4ce657af8640fad1a89ca8c71941d40e1
Wei Yan <wyan@tamu.edu>
2017-01-08 21:57:56 -0600

    Update config.js

--- www/Flux/js/config.js
+++ www/Flux/js/config.js
@@ -1,5 +1,5 @@
 const config = {
   url: window.location.href,
   flux_url: 'https://flux.io', // flux url
-  flux_client_id: 'xxxxxxxxxxxxxxxxxxxxxxxxx', // your app's client id
+  flux_client_id: 'xxxxxxxxxxxxxxxxxxxxxxxxx', // Replace 'xxxxxxxxxxxxxxxxxxxxxxxxx' with your app's client id
 }
